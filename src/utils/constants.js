154b28e3b183f3deb17c857dcb78d19c15d4efc1
Udipta <dasdsa1208@gmail.com>
2024-01-27 20:06:46 +0530

    key changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -23,4 +23,4 @@ export const SUPPORTED_LANGUAGES = [
   { identifier: "spanish", name: "Spanish" },
 ];
 
-export const OPENAI_KEY = "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";
+export const OPENAI_KEY = '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';
